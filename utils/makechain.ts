2f24ab2b4329b512a5d6fc8a570ca24d90e26350
Bliss <danbliss80@gmail.com>
2023-05-08 22:14:26 -0700

    update app with aida

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,9 +9,12 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
+// const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+// If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+// If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+If you don't know the answer just say, 'That info isn't in the AIDA Manuals' then try to make up an answer as best you can'.
 
 {context}
 
@@ -20,8 +23,8 @@ Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+    temperature: 0.2, // increase temepreature to get more creative answers
+    modelName: 'gpt-4', //change this from gpt-3.5-turbo to gpt-4 if you have access
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
